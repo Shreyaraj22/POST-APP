fa0699e1c066e01208f68a5e57fb1b9d0f163169
Shreya Raj <shreyaraj7878@gmail.com>
2025-05-19 00:15:50 +0530

    code to update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="min-h-screen bg-purple-300 py-10 px-1">
     <h1 className="text-3xl text-purple-700 font-bold text-center mb-10">
 
-         POSTS APP
+         POST APP
       </h1>
 
       <PostForm />
